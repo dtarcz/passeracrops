f8218e9840368f3e5473392cf588e6bc96d2180a
dtarcz <david@tarczewski.fr>
2017-01-16 23:16:32 +0100

    first commit

---
+++ init.lua
@@ -0,0 +1,46 @@
+-- barley
+minetest.register_alias("farming:seed_barley", "farming:seed_wheat")
+
+--blueberry
+minetest.register_alias("farming:blueberries", "air")
+
+-- bean
+minetest.register_alias("farming:beans", "crops:green_bean")
+
+--cocoa
+minetest.register_alias("farming:cocoa_beans", "air")
+minetest.register_alias("farming:cookie", "crops:uncooked_vegetable_stew") -- a remplacer plutot par des pommes !
+
+--coffee
+minetest.register_alias("farming:coffee_beans", "crops:pumpkin_seed")
+minetest.register_alias("farming:coffee_1", "crops:pumpkin_plant_1")
+minetest.register_alias("farming:coffee_2", "crops:pumpkin_plant_2")
+minetest.register_alias("farming:coffee_3", "crops:pumpkin_plant_3")
+minetest.register_alias("farming:coffee_4", "crops:pumpkin_plant_4")
+minetest.register_alias("farming:coffee_5", "crops:pumpkin_plant_5")
+
+
+
+-- tomato
+minetest.register_alias("farming:tomato", "crops:tomato")
+minetest.register_alias("farming:tomato_1",  "crops:tomato_seed")
+minetest.register_alias("farming:tomato_2", "crops:tomato_plant_1")
+minetest.register_alias("farming:tomato_3", "crops:tomato_plant_2")
+minetest.register_alias("farming:tomato_4", "crops:tomato_plant_3")
+minetest.register_alias("farming:tomato_5", "crops:tomato_plant_3")
+minetest.register_alias("farming:tomato_6", "crops:tomato_plant_4")
+minetest.register_alias("farming:tomato_7", "crops:tomato_plant5")
+
+-- carrot
+minetest.register_alias("farming:carrot", "crops:melon_slice")
+minetest.register_alias("farming:carrot_1", "crops:melon_plant_1")
+minetest.register_alias("farming:carrot_2", "crops:melon_plant_1")
+minetest.register_alias("farming:carrot_3", "crops:melon_plant_1")
+minetest.register_alias("farming:carrot_4", "crops:melon_plant_1")
+minetest.register_alias("farming:carrot_5", "crops:melon_plant_1")
+minetest.register_alias("farming:carrot_6", "crops:melon_plant_1")
+minetest.register_alias("farming:carrot_7", "crops:melon_plant_1")
+minetest.register_alias("farming:carrot_8", "crops:melon_plant_1")
+
+-- golden carrot
+minetest.register_alias("farming:carrot_gold", "air")
