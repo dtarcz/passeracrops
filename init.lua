ced73a237db530a04d8b62c37dae554c9ae97bb0
dtarcz <david@tarczewski.fr>
2017-01-17 12:13:12 +0100

    ajout de quelques cultures

--- init.lua
+++ init.lua
@@ -7,30 +7,6 @@ minetest.register_alias("farming:blueberries", "air")
 -- bean
 minetest.register_alias("farming:beans", "crops:green_bean")
 
---cocoa
-minetest.register_alias("farming:cocoa_beans", "air")
-minetest.register_alias("farming:cookie", "crops:uncooked_vegetable_stew") -- a remplacer plutot par des pommes !
-
---coffee
-minetest.register_alias("farming:coffee_beans", "crops:pumpkin_seed")
-minetest.register_alias("farming:coffee_1", "crops:pumpkin_plant_1")
-minetest.register_alias("farming:coffee_2", "crops:pumpkin_plant_2")
-minetest.register_alias("farming:coffee_3", "crops:pumpkin_plant_3")
-minetest.register_alias("farming:coffee_4", "crops:pumpkin_plant_4")
-minetest.register_alias("farming:coffee_5", "crops:pumpkin_plant_5")
-
-
-
--- tomato
-minetest.register_alias("farming:tomato", "crops:tomato")
-minetest.register_alias("farming:tomato_1",  "crops:tomato_seed")
-minetest.register_alias("farming:tomato_2", "crops:tomato_plant_1")
-minetest.register_alias("farming:tomato_3", "crops:tomato_plant_2")
-minetest.register_alias("farming:tomato_4", "crops:tomato_plant_3")
-minetest.register_alias("farming:tomato_5", "crops:tomato_plant_3")
-minetest.register_alias("farming:tomato_6", "crops:tomato_plant_4")
-minetest.register_alias("farming:tomato_7", "crops:tomato_plant_5")
-
 -- carrot
 minetest.register_alias("farming:carrot", "crops:melon_slice")
 minetest.register_alias("farming:carrot_1", "crops:melon_plant_1")
@@ -44,3 +20,68 @@ minetest.register_alias("farming:carrot_8", "crops:melon_plant_1")
 
 -- golden carrot
 minetest.register_alias("farming:carrot_gold", "air")
+
+--cocoa
+minetest.register_alias("farming:cocoa_beans", "air")
+minetest.register_alias("farming:cookie", "crops:uncooked_vegetable_stew") -- a remplacer plutot par des pommes !
+
+--coffee
+minetest.register_alias("farming:coffee_beans", "crops:pumpkin_seed")
+minetest.register_alias("farming:coffee_1", "crops:pumpkin_plant_1")
+minetest.register_alias("farming:coffee_2", "crops:pumpkin_plant_2")
+minetest.register_alias("farming:coffee_3", "crops:pumpkin_plant_3")
+minetest.register_alias("farming:coffee_4", "crops:pumpkin_plant_4")
+minetest.register_alias("farming:coffee_5", "crops:pumpkin_plant_5")
+
+--corn
+minetest.register_alias("farming:corn", "crops:corn_cob")
+minetest.register_alias("farming:corn_cob", "crops:corn_cob")
+minetest.register_alias("farming:corn_1", "crops:corn_base_seed")
+minetest.register_alias("farming:corn_2", "crops:corn_base_seed")
+minetest.register_alias("farming:corn_3", "crops:corn_base_1")
+minetest.register_alias("farming:corn_4", "crops:corn_base_1")
+minetest.register_alias("farming:corn_5", "crops:corn_base_2")
+minetest.register_alias("farming:corn_6", "crops:corn_base_2")
+minetest.register_alias("farming:corn_7", "crops:corn_base_3")
+minetest.register_alias("farming:corn_8", "crops:corn_base_3")
+
+--cucumber
+minetest.register_alias("farming:cucumber", "crops:melon_slice")
+minetest.register_alias("farming:cucumber_1", "air")
+minetest.register_alias("farming:cucumber_2", "air")
+minetest.register_alias("farming:cucumber_3", "air")
+minetest.register_alias("farming:cucumber_4", "air")
+
+--grapes
+minetest.register_alias("farming:grapes", "crops:green_bean")
+minetest.register_alias("farming:grapes_1", "air")
+minetest.register_alias("farming:grapes_2", "air")
+minetest.register_alias("farming:grapes_3", "air")
+minetest.register_alias("farming:grapes_4", "air")
+minetest.register_alias("farming:grapes_5", "air")
+minetest.register_alias("farming:grapes_6", "air")
+minetest.register_alias("farming:grapes_7", "air")
+minetest.register_alias("farming:grapes_8", "air")
+minetest.register_alias("farming:grapebush", "air")
+
+-- melon
+minetest.register_alias("farming:melon_slice", "crops:melon_slice")
+minetest.register_alias("farming:melon_1", "crops:melon_plant_1")
+minetest.register_alias("farming:melon_2", "crops:melon_plant_2")
+minetest.register_alias("farming:melon_3", "crops:melon_plant_2")
+minetest.register_alias("farming:melon_4", "crops:melon_plant_3")
+minetest.register_alias("farming:melon_5", "crops:melon_plant_3")
+minetest.register_alias("farming:melon_6", "crops:melon_plant_3")
+minetest.register_alias("farming:melon_7", "crops:melon_plant_4")
+minetest.register_alias("farming:melon_8", "crops:melon_plant_4")
+
+-- tomato
+minetest.register_alias("farming:tomato", "crops:tomato")
+minetest.register_alias("farming:tomato_1",  "crops:tomato_seed")
+minetest.register_alias("farming:tomato_2", "crops:tomato_plant_1")
+minetest.register_alias("farming:tomato_3", "crops:tomato_plant_2")
+minetest.register_alias("farming:tomato_4", "crops:tomato_plant_3")
+minetest.register_alias("farming:tomato_5", "crops:tomato_plant_3")
+minetest.register_alias("farming:tomato_6", "crops:tomato_plant_4")
+minetest.register_alias("farming:tomato_7", "crops:tomato_plant_5")
+
